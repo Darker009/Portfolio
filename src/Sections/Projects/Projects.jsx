e1a60b7e18224f911d8c1d239a93d438c5aa7aec
Darker <bhosalesudarshan9@gmail.com>
2025-03-11 22:16:36 +0530

    project added

--- src/Sections/Projects/Projects.jsx
+++ src/Sections/Projects/Projects.jsx
@@ -1,5 +1,6 @@
 import styles from '../Projects/ProjectsStyle.module.css';
-import viberr from '../../assets/viberr.png';
+import viberr01 from '../../assets/viberr.png';
+import viberr02 from '../../assets/ETS.png';
 import ProjectCard from '../../Common/ProjectCard';
 
 function Projects() {
@@ -8,9 +9,18 @@ function Projects() {
       <h1 className="sectionTitle">Projects</h1>
       <div className={styles.projectsContainer}>
         <ProjectCard
-          src={viberr}
+          src={viberr01}
           link="https://github.com/Darker009/Employee-Management-System"
           h3="Employee Management"
+          p="Deployed"
+        />
+      </div>
+<br /><br />
+      <div className={styles.projectsContainer}>
+        <ProjectCard
+          src={viberr02}
+          link="https://github.com/Darker009/ExpenseTrackerSystem"
+          h3="Expense Tracker System"
           p="Deployed"
         />
       </div>
